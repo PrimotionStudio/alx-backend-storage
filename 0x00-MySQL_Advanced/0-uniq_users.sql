f553b728e76dccc7af680a943c6c8b94a055cc90
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-12 10:19:28 +0100

    "Removed redundant comment and corrected 'EXIST' to 'EXISTS' and 'NOT EXIST' to no keyword in CREATE TABLE statement."

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,6 @@
 --We are all unique
---We are all unique
 DROP TABLE IF EXIST users;
-CREATE TABLE IF NOT EXIST users (
+CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
