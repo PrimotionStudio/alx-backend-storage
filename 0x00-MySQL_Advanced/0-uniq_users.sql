d1df08277514678feeec8f1b12abbe97cf5f24a0
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-12 06:58:21 +0100

    "Added DROP TABLE statement and removed duplicate comment in 0-uniq_users.sql"

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,6 @@
 --We are all unique
 --We are all unique
+DROP TABLE IF EXIST users;
 CREATE TABLE IF NOT EXIST users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
