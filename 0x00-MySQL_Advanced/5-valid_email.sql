d764395281f19f450a8be3251ec8f32bef1ee684
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-12 22:58:39 +0100

    "Added email validation trigger to users table"

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- Email validation to sent
+DELIMITER //
+DROP TRIGGER IF EXISTS email_validation//
+CREATE TRIGGER email_validation
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+        SET NEW.valid_email = 0
+    END IF;
+END//
+
+DELIMITER ;
+
