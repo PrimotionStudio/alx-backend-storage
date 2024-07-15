bb6aac6126d63e7094be6c8f626a6c57551ee240
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-15 22:14:29 +0100

    0

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,1 +1,35 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+provides some stats about Nginx logs stored in MongoDB
+"""
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    col_nginx = client.logs.nginx
+    total = col_nginx.count_documents({})
+    print(f'{total} logs')
+    print("Methods:")
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for met in method:
+        print(f'\tmethod {met}: {col_nginx.count_documents({"method": met})}')
+    status_total = col_nginx.count_documents({"path": "/status"})
+    print(f'{status_total} status check')
+    print("IPs:")
+    top_ten_ips = col_nginx.aggregate([
+        {
+            "$group": {
+                "_id": "$ip",
+                "count": { "$sum": 1 }
+            }
+        },
+        {
+            "$sort": { "count": -1 }
+        },
+        {
+            "$limit": 10
+        }
+    ])
+    for top_ten in top_ten_ips:
+        print(f"\t{top_ten["_id"]}: {top_ten["count"]}")
