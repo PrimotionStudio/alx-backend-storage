6383545284523fcdd8c83402926b0691f9e83fe5
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-15 21:01:25 +0100

    "Added a description to the script, removed empty lines at the end of the file"

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """
+provides some stats about Nginx logs stored in MongoDB
 """
 from pymongo import MongoClient
 
@@ -14,6 +15,4 @@ if __name__ == "__main__":
     for met in method:
         print(f'\tmethod {met}: {col_nginx.count_documents({"method": met})}')
     status_total = col_nginx.count_documents({"path": "/status"})
-    print(f'{status_total} status check')
-
-    
+    print(f'{status_total} status check')    
