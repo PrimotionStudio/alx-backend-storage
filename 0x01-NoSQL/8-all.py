a9319dba6dd497ba2a8b1e2a9e2a903c2c197f97
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-15 20:01:18 +0100

    "Added docstring and function to list all documents in a MongoDB collection"

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+Python function that lists all
+documents in a collection
+"""
+
+def list_all(mongo_collection):
+    """
+    Python function that lists all
+    documents in a collection
+    """
+    return mongo_collection.find()
