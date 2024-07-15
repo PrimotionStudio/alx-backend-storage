3823e7f15dd08e82b16eb58ad45b35d62729eccb
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-15 20:58:27 +0100

    "Updated log_stats.py to connect to MongoDB and query log data; modified shebang line and added code to count logs, methods, and status checks. Also updated 9-insert_school.cpython-312.pyc binary file."

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,1 +1,19 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+"""
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    col_nginx = client.logs.nginx
+    total = col_nginx.count_documents({})
+    print(f'{total} logs')
+    print("Methods:")
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for met in method:
+        print(f'\tmethod {met}: {col_nginx.count_documents({"method": met})}')
+    status_total = col_nginx.count_documents({"path": "/status"})
+    print(f'{status_total} status check')
+
+    
