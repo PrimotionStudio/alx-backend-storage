d0722f0189251cd5d54796df76ad64bba25e4d65
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-15 22:01:13 +0100

    "Updated 101-students.py to use env python3 and added aggregation pipeline to sort students by average score; updated __pycache__/9-insert_school.cpython-312.pyc"

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,1 +1,25 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+returns all students sorted by average score
+"""
+
+def top_students(mongo_collection):
+    """
+    returns all students sorted by average score
+    """
+    return mongo_collection.aggregate([
+        {
+            "$project": {
+                "name": 1,
+                "topics": 1,
+                "averageScore": {
+                    "$avg": "$topics.score"
+                }
+            }
+        },
+        {
+            "$sort": {
+                "averageScore": -1
+            }
+        }
+    ])
