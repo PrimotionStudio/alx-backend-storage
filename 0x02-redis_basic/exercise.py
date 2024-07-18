65bf59ee70874198e58e170b512bb2c18cc7ef35
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-18 03:48:57 +0100

    "Added get method to Cache class with optional callback function, and two convenience methods get_int and get_str."

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@ of all the files in a directory
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -18,7 +18,7 @@ class Cache:
         """
         self._redis = redis.Redis()
         self._redis.flushdb()
-    
+
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         This method is used to store data in the cache
@@ -26,3 +26,26 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """
+        This method is used to get data from the cache
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_int(self, key: str) -> int:
+        """
+        This method is used to get an integer from the cache
+        """
+        return self.get(key, int)
+
+    def get_str(self, key: str) -> str:
+        """
+        This method is used to get a string from the cache
+        """
+        return self.get(key, lambda d: d.decode('utf-8'))
